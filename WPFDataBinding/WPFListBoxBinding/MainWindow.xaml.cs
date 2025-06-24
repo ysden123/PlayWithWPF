aa16c1dc60aa024543d5ad64137e0fa50ba53573
Yuriy Stul <ysden123@gmail.com>
2025-06-24 11:35:32 +0300

    ListView data binding example

---
+++ WPFDataBinding/WPFListBoxBinding/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WPFListBoxBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly ObservableCollection<Person> Persons = [
+            new Person {FirstName = "Ivan", LastName = "Petrov", Age = 12},
+            new Person {FirstName = "Semen", LastName = "Putov", Age = 21},
+            new Person {FirstName = "Petr", LastName = "Tonkov", Age = 14},
+            new Person {FirstName = "Uhbu", LastName = "Maliy", Age = 9}
+            ];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ListBox_Persons.ItemsSource = Persons;
+        }
+    }
+}
---
+++ WPFDataBinding/WPFListViewBinding/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFListViewBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly ObservableCollection<Person> Persons = [
+            new Person {FirstName = "Ivan", LastName = "Petrov", Age = 12},
+            new Person {FirstName = "Semen", LastName = "Putov", Age = 21},
+            new Person {FirstName = "Petr", LastName = "Tonkov", Age = 14},
+            new Person {FirstName = "Uhbu", LastName = "Maliy", Age = 9}
+            ];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ListView_Persons.ItemsSource = Persons;
+        }
+    }
+}
