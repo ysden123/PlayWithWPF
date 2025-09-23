bdec20e5e5c138456eb1c27c6b34d7c3b201cd9b
Yuriy Stul <ysden123@gmail.com>
2025-09-23 19:21:29 +0300

    Implementation of INotifyPropertyChanged with binding in WPF

---
+++ WPFDataBinding/WPFINotifyPropertyChanged/Item.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace WPFINotifyPropertyChanged
+{
+    public class Item : INotifyPropertyChanged
+    {
+        private string? _column1;
+        private string? _column2;
+
+        public string? Column1
+        {
+            get { return _column1; }
+            set
+            {
+                _column1 = value;
+                OnPropertyChanged(nameof(Column1));
+            }
+        }
+
+        public string? Column2
+        {
+            get { return _column2; }
+            set
+            {
+                _column2 = value;
+                OnPropertyChanged(nameof(Column2));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WPFDataBinding/WPFINotifyPropertyChanged/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WPFINotifyPropertyChanged
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Items = new ObservableCollection<Item>();
+            // Добавьте тестовые данные
+            Items.Add(new Item { Column1 = "Значение 1", Column2 = "Значение 2" });
+            Items.Add(new Item { Column1 = "Значение 3", Column2 = "Значение 4" });
+
+            // Привяжите коллекцию к DataContext
+            this.DataContext = this;
+        }
+
+        private void ChangeButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (Items.Count > 0)
+            {
+                // Изменение значения в первом элементе
+                Items[0].Column2 = "Новое значение";
+            }
+        }
+    }
+}
